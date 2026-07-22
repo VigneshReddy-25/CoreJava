6940f95cf09331991c3f74a067bc488049228410
Vignesh Reddy <reddyvignesh731@gmail.com>
2026-07-22 19:02:41 +0530

    Add Car class

---
+++ src/Encapsulation/Car.java
@@ -0,0 +1,22 @@
+package Encapsulation;
+
+public class Car {
+	private String model;
+	private int year;
+	
+	public void setModel(String model) {
+		this.model=model;		
+	}
+	public String getModel(){
+		return model;
+	}
+	
+	public void setYear(int year) {
+		this.year=year;
+	}
+	public int getYear() {
+		return year;
+	}
+
+
+}
