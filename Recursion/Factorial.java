ee5de422ece732784211fe9af1316c8ab9e338ee
Vignesh Reddy <reddyvignesh731@gmail.com>
2026-07-30 19:15:56 +0530

    Add Factorial class

---
+++ Recursion/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(String args[]){
+    int a=5;
+    System.out.println(fact(a));
+  }
+  public static int fact(int a){
+    if(a==1){
+      return 1;
+    }
+    return a*fact(a-1);
+  }
+}
