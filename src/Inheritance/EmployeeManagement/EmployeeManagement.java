c2232d03d63aad9f47b5cedb86b984cb60c9d56f
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-08-05 22:03:38 +0530

    Implement EmployeeManagement class with main method

--- src/Inheritance/EmployeeManagement/EmployeeManagement.java
+++ src/Inheritance/EmployeeManagement/EmployeeManagement.java
@@ -1,1 +1,12 @@
+package Inheritance;
+public class EmployeeManagement {
 
+	public static void main(String args[]) {
+		Developer dev=new Developer(101,"Vignesh",60000,"Java","Employee Management");
+		Tester test =new Tester(102,"Ravi",40000,"Selenium","Selenium");
+		System.out.println("Developer Details: ");
+		dev.displayDetails();
+		System.out.println("Testing Details: ");
+		test.displayDetails();
+	}
+}
