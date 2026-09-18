f39057f59d450a35469fba0d92fe9e9c31463514
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-18 22:31:45 +0530

    Add NoStudents exception class

---
+++ src/StudentManagement/src/Exceptions/NoStudents.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class NoStudents extends RuntimeException {
+
+	public NoStudents(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
