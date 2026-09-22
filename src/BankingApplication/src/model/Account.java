059e465ee5981efe9f0f912e84aa292cf7c3ae9e
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-22 21:15:14 +0530

    Update Account class to include Customer object
    Refactored Account class to use Customer object instead of account holder name.

--- src/BankingApplication/src/model/Account.java
+++ src/BankingApplication/src/model/Account.java
@@ -1,26 +1,24 @@
-package model;
-
+package model;	
 public class Account {
 
 	private long accountNumber;
-    private String accountHolderName;
+	private Customer customer;
+//    private String accountHolderName;
     private double balance;
     
-    public Account() {
-    	
-    }
     
-    public Account(long accountNumber,String accountHolderName) {
+    
+    public Account(long accountNumber,Customer customer) {
     	this.accountNumber=accountNumber;
-    	this.accountHolderName=accountHolderName;
+    	this.customer=customer;
     	this.balance=0;
     }
     
     public long getAccountNumber() {
     	return accountNumber;
     }
-    public String getAccountHolderName() {
-    	return accountHolderName;
+    public Customer getCustomer() {
+    	return customer;
     }
     public double getBalance() {
     	return balance;
@@ -29,16 +27,25 @@ public class Account {
     public void setAccountNumber(long accountNumber) {
     	this.accountNumber=accountNumber;
     }
-    public void setAccountHolderName(String accountHolderName) {
-    	this.accountHolderName=accountHolderName;
+    public void setCustomer(Customer customer) {
+    	this.customer=customer;
     }
     public void setBalance(double balance) {
         this.balance = balance;
     }
+    
+    public boolean withdraw(double amount) {
+        if (amount <= 0 || amount > balance) {
+            return false;
+        }
+
+        balance = balance - amount;
+        return true;
+    }
 	
     public String displayAccount() {
     	return "Account Number: "+ accountNumber
-    			+" Account Holder Name: "+ accountHolderName +
+    			+" Account Holder Name: "+ customer.getName() +
     			" Balance: "+balance;
     }
 	
